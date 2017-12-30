14386e93eb855fafb6ed2e618e43436294766c54
camberlucian <cambergriffin@gmail.com>
2017-12-30 12:54:54 -0500

    completed dice roller

--- dndMain.py
+++ dndMain.py
@@ -2,75 +2,91 @@ import sys
 import random
 
 def diceRoller():
-	print("+++++++++++++++++++++++++++++++")
-	print("----------DICE ROLLER----------")
-	print("+++++++++++++++++++++++++++++++")
-	print("1. ROLL 20")
-	print("2. ROLL 12")
-	print("3. ROLL 10")
-	print("4. ROLL  8")
-	print("5. ROLL  6")
-	print("6. ROLL  4")
-	print("7. FLIP COIN")
-	print("8. EXIT")
-	print("+++++++++++++++++++++++++++++++")
 	goodSelection = False
 	selection = ""
 	while goodSelection == False:
-	    selection = input("ENTER NUMERIC COMMAND:  ")
-	    try:
-	    	command = int(selection)
-	    	if command == 1:
-	    		print("roll20")
-	    		die = 20
-	    	elif command == 2:
-	    		die = 12
-	    	elif command == 3:
-	    		die = 10
-	    	elif command == 4:
-	    		die = 8
-	    	elif command == 5:
-	    		die = 6
-	    	elif command == 6:
-	    		die = 4
-	    	elif command == 7:
-	    		die = 2
-	    	elif command == 8:
-	    		print("exiting")
-	    		goodSelection = True
-	    	print("lets do this")
-	    	if goodSelection == False:
-	    		print("roll")
-	    		roll(die)
-	    		print("done rollin")
+		print("+++++++++++++++++++++++++++++++")
+		print("----------DICE ROLLER----------")
+		print("+++++++++++++++++++++++++++++++")
+		print("1. ROLL 20")
+		print("2. ROLL 12")
+		print("3. ROLL 10")
+		print("4. ROLL  8")
+		print("5. ROLL  6")
+		print("6. ROLL  4")
+		print("7. FLIP COIN")
+		print("8. EXIT")
+		print("+++++++++++++++++++++++++++++++")
+		selection = input("ENTER NUMERIC COMMAND:  ")
+		try:
+			command = int(selection)
+			if command == 1:
+				print("roll20")
+				die = 20
+			elif command == 2:
+				die = 12
+			elif command == 3:
+				die = 10
+			elif command == 4:
+				die = 8
+			elif command == 5:
+				die = 6
+			elif command == 6:
+				die = 4
+			elif command == 7:
+				die = 2
+			elif command == 8:
+				print("exiting")
+				goodSelection = True
+			if goodSelection == False:
+				roll(die)
+				print("done rollin")
 
-	    except:
-	    	goodSelection = False
+		except:
+			goodSelection = False
 
 def roll(die):
-	print("in roll die")
 	done = False
 	rolls = []
 	while done == False:
-		print("in loop")
 		num = random.randint(1,die)
-		print("random number got " + num)
 		rolls.append(num)
 		response = "ROLLED: "
 		total = 0
 		for number in rolls:
-			response += number + " "
+			response += str(number) + " "
 			total += number
 		if len(rolls) > 1:
-			response += " - TOTAL: " + total
-		print(response)
+			try:
+			    response += " - TOTAL: " + str(total)
+			except Exception, e:
+				print("bad response")
+				print(str(e))
 		properAnswer = False
 		again = ""
 		properAnswers = ["y", "n", "yes", "no"]
-		while again.lower() not in properAnswers:
-			again = input("ROLL AGAIN? Y/N")
-		if again.lower() in ["y", "yes"]:
-		    done == True
+		try:
+		    again = raw_input("ROLL AGAIN? Y/N : ")
+		except Exception, e:
+			print("something didnt work")
+			print(str(e))
+		if again.lower() in ["n", "no"]:
+			done = True
+
+def rollSome(die, num, mod=0):
+	rolls = []
+	result = "ROLLED: "
+	total = 0
+	for x in range(0, num):
+		roll = random.randint(1,die) + mod
+		rolls.append(roll)
+	for number in rolls:
+		result += str(number) + " "
+		total += number
+	result += " - TOTAL: " + str(total)
+	print(result)
+
+
 
 def main():
 	print("+++++++++++++++++++++++++++++++")
@@ -83,35 +99,37 @@ def main():
 	return
 
 def mainMenu():
-	print("+++++++++++++++++++++++++++++++")
-	print("-----------MAIN MENU-----------")
-	print("+++++++++++++++++++++++++++++++")
-	print("1. CREATE CHARACTER (NOT DONE)")
-	print("2. PLAY WITH EXISTING CHARACTER (NOT DONE)")
-	print("3. EDIT CHARACTER (NOT DONE")
-	print("4. SIMPLE DICE ROLLER (NOT DONE)")
-	print("5. EXIT")
-	print("+++++++++++++++++++++++++++++++")
 	goodSelection = False
 	selection = ""
 	while goodSelection == False:
-	    selection = input("ENTER NUMERIC COMMAND:  ")
-	    try:
-	    	command = int(selection)
-	    	if command == 1:
-	    		print("creating character")
-	    	elif command == 2:
-	    		print("load character")
-	    	elif command == 3:
-	    		print("edit character")
-	    	elif command == 4:
-	    		diceRoller()
-	    	elif command == 5:
-	    		print("exiting")
-	    		goodSelection = True
+		print("+++++++++++++++++++++++++++++++")
+		print("-----------MAIN MENU-----------")
+		print("+++++++++++++++++++++++++++++++")
+		print("1. CREATE CHARACTER (NOT DONE)")
+		print("2. PLAY WITH EXISTING CHARACTER (NOT DONE)")
+		print("3. EDIT CHARACTER (NOT DONE")
+		print("4. SIMPLE DICE ROLLER (NOT DONE)")
+		print("5. EXIT")
+		print("+++++++++++++++++++++++++++++++")
 
-	    except:
-	    	goodSelection = False
+
+		selection = input("ENTER NUMERIC COMMAND:  ")
+		try:
+			command = int(selection)
+			if command == 1:
+				print("creating character")
+			elif command == 2:
+				print("load character")
+			elif command == 3:
+				print("edit character")
+			elif command == 4:
+				diceRoller()
+			elif command == 5:
+				print("exiting")
+				goodSelection = True
+
+		except:
+			goodSelection = False
 
 if __name__ == '__main__':
 	main()
